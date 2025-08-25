367db697a5a59fba7913294bb31c4d8bf1a9e4b8
Michaelninder <fabianeternis@gmail.com>
2025-08-25 18:01:26 +0200

    .

--- resources/views/profile/partials/avatar-form.blade.php
+++ resources/views/profile/partials/avatar-form.blade.php
@@ -16,8 +16,13 @@
         @method('patch')
 
         <div>
-            <x-input-label for="avatar" :value="__('Avatar')" />
-            <input type="file" id="avatar" name="avatar" class="mt-1 block w-full" />
+            <x-input-label for="avatar" :value="__('Upload Avatar')" />
+            <input
+                id="avatar"
+                name="avatar"
+                type="file"
+                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            />
             <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
         </div>
 
