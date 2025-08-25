deb0787e066b62b36eae81dd0bd2b4baf6446f69
Michaelninder <fabianeternis@gmail.com>
2025-08-25 18:37:20 +0200

    !?

--- resources/views/profile/partials/avatar-form.blade.php
+++ resources/views/profile/partials/avatar-form.blade.php
@@ -21,7 +21,11 @@
                 id="avatar"
                 name="avatar"
                 type="file"
-                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                class="form-input file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100 dark:file:bg-violet-900 dark:file:text-violet-300 dark:hover:file:bg-violet-800
+                       block w-full text-sm text-gray-900 dark:text-gray-400
+                       border border-gray-300 dark:border-gray-600 rounded-lg cursor-pointer
+                       bg-gray-50 dark:bg-gray-700
+                       focus:outline-none focus:ring focus:ring-violet-200 dark:focus:ring-violet-700"
             />
             <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
         </div>
