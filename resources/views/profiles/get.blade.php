71173a72315e648a14c1ab1977ed2905523de49b
Michaelninder <fabianeternis@gmail.com>
2025-09-06 17:07:16 +0200

    feat: fix something withscroll-mt-16 ...

--- resources/views/profiles/get.blade.php
+++ resources/views/profiles/get.blade.php
@@ -180,7 +180,7 @@
             </div>
         </header>
 
-        <main id="top" class="flex-1">
+        <main id="top" class="flex-1 scroll-mt-16">
             <section class="py-10 sm:py-12">
                 <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="grid grid-cols-1 md:grid-cols-[auto,1fr] items-center gap-8">
