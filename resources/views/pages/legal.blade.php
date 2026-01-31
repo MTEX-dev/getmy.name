9b6e8b1b8de6b7efa7b201b8b97941a6e686e1c2
Michaelninder <fabianeternis@gmail.com>
2026-01-31 12:29:01 +0100

    feat: Chnage colot on pages.legal

--- resources/views/pages/legal.blade.php
+++ resources/views/pages/legal.blade.php
@@ -15,7 +15,7 @@
                         @foreach ($sections as $_section)
                             <a href="{{ route('legal', ['section' => $_section]) }}"
                                 class="py-2 px-4 text-sm font-medium
-                                {{ $_section == $section ? 'text-blue-600 border-b-2 border-blue-600' : 'text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200' }}">
+                                {{ $_section == $section ? 'text-getmyname-600 border-b-2 border-getmyname-600' : 'text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200' }}">
                                 {{ __('legal.' . $_section . '.tab_title') }}
                             </a>
                         @endforeach
