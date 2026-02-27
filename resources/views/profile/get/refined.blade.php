5b73104e1124518cc84d556cb156cf4d6735da5d
Michaelninder <fabianeternis@gmail.com>
2026-02-27 22:09:02 +0100

    feat: Update profile.get.default and profile.get.refined by using Lucide-Icons instead of Bootstrap-Icons ...

--- resources/views/profile/get/refined.blade.php
+++ resources/views/profile/get/refined.blade.php
@@ -11,89 +11,90 @@
         }
     };
     
+    // Refactored to use Lucide icon names
     $socials = [
         'github' => [
-            'svg' => '<path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.43.372.823 1.102.823 2.222 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>',
+            'icon' => 'github',
             'label' => 'GitHub',
             'prefix' => 'https://github.com/',
         ],
         'linkedin' => [
-            'svg' => '<path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>',
+            'icon' => 'linkedin',
             'label' => 'LinkedIn',
             'prefix' => 'https://www.linkedin.com/in/',
         ],
         'twitter' => [
-            'svg' => '<path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/>',
+            'icon' => 'twitter',
             'label' => 'X',
             'prefix' => 'https://x.com/',
         ],
         'bluesky' => [
-            'svg' => '<path d="M12 10.8c-1.324-1.896-4.602-5.493-7.861-6.156C2.779 4.369 0 5.645 0 8.073c0 1.474.59 3.372 1.846 4.937.45.557.94 1.076 1.277 1.419-.738 1.236-1.554 3.13-1.554 4.886 0 3.332 1.492 5.23 5.631 5.481 2.24.135 3.388-1.298 4.793-2.79.141-.141.282-.282.427-.419.145.137.286.278.427.419 1.405 1.492 2.553 2.925 4.793 2.79 4.139-.251 5.631-2.149 5.631-5.481 0-1.756-.816-3.65-1.554-4.886.337-.343.827-.862 1.277-1.419C23.41 11.445 24 9.547 24 8.073c0-2.428-2.779-3.704-4.139-3.429-3.259.663-6.537 4.26-7.861 6.156Z"/>',
+            'icon' => 'cloud', // Lucide generic
             'label' => 'Bluesky',
             'prefix' => 'https://bsky.app/profile/',
         ],
         'personal_website' => [
-            'svg' => '<path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"/>',
+            'icon' => 'globe',
             'label' => 'Website',
             'prefix' => '',
         ],
         'codepen' => [
-            'svg' => '<path fill-rule="evenodd" clip-rule="evenodd" d="M11.4265 1.18077C11.7709 0.939744 12.2291 0.939744 12.5735 1.18077L22.5735 8.18077C22.8408 8.3679 23 8.67369 23 9V16C23 16.3263 22.8408 16.6321 22.5735 16.8192L12.5735 23.8192C12.2291 24.0603 11.7709 24.0603 11.4265 23.8192L1.42654 16.8192C1.15921 16.6321 1 16.3263 1 16V9C1 8.67369 1.15921 8.3679 1.42654 8.18077L11.4265 1.18077ZM3 10.9207V14.0793L5.25621 12.5L3 10.9207ZM7 13.7207L3.74379 16L11 21.0793V16.5207L7 13.7207ZM13 16.5207V21.0793L20.2562 16L17 13.7207L13 16.5207ZM18.7438 12.5L21 14.0793V10.9207L18.7438 12.5ZM20.2562 9L17 11.2793L13 8.47934V3.92066L20.2562 9ZM11 3.92066V8.47934L7 11.2793L3.74379 9L11 3.92066ZM12 10.2207L8.74379 12.5L12 14.7793L15.2562 12.5L12 10.2207Z"/>',
+            'icon' => 'pen-square', // Lucide generic
             'label' => 'CodePen',
             'prefix' => 'https://codepen.io/',
         ],
         'instagram' => [
-            'svg' => '<path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>',
+            'icon' => 'instagram',
             'label' => 'Instagram',
             'prefix' => 'https://www.instagram.com/',
         ],
         'youtube_url' => [
-            'svg' => '<path d="M23.498 6.186c-.275-1.031-1.082-1.838-2.113-2.113C19.518 3.545 12 3.545 12 3.545s-7.518 0-9.385.528c-1.031.275-1.838 1.082-2.113 2.113C0 8.053 0 12 0 12s0 3.947.502 5.814c.275 1.03 1.082 1.837 2.113 2.113C4.482 20.455 12 20.455 12 20.455s7.518 0 9.385-.528c1.03-.276 1.837-1.083 2.113-2.113C24 15.947 24 12 24 12s0-3.947-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>',
+            'icon' => 'youtube',
             'label' => 'YouTube',
             'prefix' => '',
         ],
         'stackoverflow' => [
-            'svg' => '<path d="M12.412 14.572V10.29h1.428V16H1v-5.71h1.428v4.282z"/><path d="M3.857 13.145h7.137v-1.428H3.857zM10.254 0 9.108.852l4.26 5.727 1.146-.852zm-3.54 3.377 5.484 4.567.913-1.097L7.627 2.28l-.914 1.097zM4.922 6.55l6.47 3.013.603-1.294-6.47-3.013zm-.925 3.344 6.985 1.469.294-1.398-6.985-1.468z"></path>',
+            'icon' => 'layers', // Lucide generic
             'label' => 'Stack Overflow',
             'prefix' => 'https://stackoverflow.com/users/',
         ],
         'dev_to' => [
-            'svg' => '<path d="M7.42 10.05c-.18-.16-.46-.23-.84-.23H6l.02 4.42h.66c.51 0 .88-.13 1.11-.39.22-.26.33-.6.33-1.05v-1.7c0-.46-.1-.81-.3-1.05zM24 12c0 6.627-5.373 12-12 12S0 18.627 0 12 5.373 0 12 0s12 5.373 12 12zm-14.59 4.14a3.35 3.35 0 0 0 1.16-.31 2.3 2.3 0 0 0 .84-.73 2.5 2.5 0 0 0 .42-1.02c.08-.43.12-.83.12-1.2V11c0-.37-.04-.77-.12-1.2a2.5 2.5 0 0 0-.42-1.02 2.3 2.3 0 0 0-.84-.73 3.35 3.35 0 0 0-1.16-.31c-.41-.02-1.24-.02-1.43-.02H4v7.46h1.98c.19 0 1.02 0 1.43-.02zM15 8.54h-1.47v7.46H15v-2.45h1.16v-1.5H15V10h1.56V8.54H15zm5.54 0h-1.33L18 10.51l-1.21-1.97H15.4l2 3.2v4.29h1.34v-4.29l2-3.21z"/>',
+            'icon' => 'terminal-square', // Lucide generic
             'label' => 'DEV',
             'prefix' => 'https://dev.to/',
         ],
         'hashnode' => [
-            'svg' => '<path d="M22.351 8.019l-6.37-6.37a5.63 5.63 0 0 0-7.962 0l-6.37 6.37a5.63 5.63 0 0 0 0 7.962l6.37 6.37a5.63 5.63 0 0 0 7.962 0l6.37-6.37a5.63 5.63 0 0 0 0-7.962zM12 15.953a3.953 3.953 0 1 1 0-7.906 3.953 3.953 0 0 1 0 7.906z"/>',
+            'icon' => 'newspaper', // Lucide generic
             'label' => 'Hashnode',
             'prefix' => 'https://hashnode.com/@',
         ],
         'npm' => [
-            'svg' => '<path d="M1.493 0v24h21.014V0H1.493zM5.96 21.037H3.985v-18.06h16.03v18.06h-3.986V7.485h-4.015v13.552H5.96v.001z"/>',
+            'icon' => 'package', // Lucide generic
             'label' => 'npm',
             'prefix' => 'https://www.npmjs.com/~',
         ],
         'product_hunt' => [
-            'svg' => '<path d="M13.56 12.348h-2.31V9.112h2.31c.893 0 1.617.724 1.617 1.618s-.724 1.618-1.617 1.618zM24 12c0 6.627-5.373 12-12 12S0 18.627 0 12 5.373 0 12 0s12 5.373 12 12zm-8.823-3.618H10.125v7.551h1.125v-2.398h2.31c2.109 0 3.817-1.709 3.817-3.818s-1.708-3.817-3.817-3.817z"/>',
+            'icon' => 'rocket', // Lucide generic
             'label' => 'Product Hunt',
             'prefix' => 'https://www.producthunt.com/@',
         ],
         'polywork' => [
-            'svg' => '<path d="M19.125 0H4.875A4.875 4.875 0 0 0 0 4.875v14.25A4.875 4.875 0 0 0 4.875 24h14.25A4.875 4.875 0 0 0 24 19.125V4.875A4.875 4.875 0 0 0 19.125 0zM12 17.5a5.5 5.5 0 1 1 5.5-5.5 5.5 5.5 0 0 1-5.5 5.5z"/>',
+            'icon' => 'layout-grid', // Lucide generic
             'label' => 'Polywork',
             'prefix' => 'https://www.polywork.com/',
         ],
         'gitlab' => [
-            'svg' => '<path d="M23.955 13.587l-1.342-4.135-2.664-8.189c-.135-.417-.724-.417-.859 0L16.425 9.45H7.574L4.911 1.263c-.135-.417-.724-.417-.859 0L1.387 9.45.045 13.587c-.114.351.015.738.317.957l11.638 8.448 11.638-8.448c.302-.219.431-.606.317-.957z"/>',
+            'icon' => 'git-branch', // Lucide generic (GitLab not in default set)
             'label' => 'GitLab',
             'prefix' => 'https://gitlab.com/',
         ],
         'dribbble' => [
-            'svg' => '<path d="M12 24C5.385 24 0 18.615 0 12S5.385 0 12 0s12 5.385 12 12-5.385 12-12 12zm10.12-10.358c-.35-.11-1.79-.53-3.646-.243a11.367 11.367 0 0 1 2.088 6.003 10.212 10.212 0 0 0 1.558-5.76zM19.08 21.23a9.637 9.637 0 0 0-2.131-5.732c-.07.068-.137.14-.203.208-2.1 2.11-4.887 4.966-5.482 9.564a10.222 10.222 0 0 0 7.816-4.04zM8.344 23.224c.614-4.535 3.1-7.14 5.216-9.261l.174-.174c-.453-1.19-.97-2.408-1.536-3.605-4.31 1.315-9.339 1.32-9.669 1.32-.03.41-.048.826-.048 1.246a10.224 10.224 0 0 0 5.863 10.474zM2.512 10.11c.31.003 4.478.019 8.52-1.032a14.85 14.85 0 0 0-3.375-5.915 10.227 10.227 0 0 0-5.145 6.947zm7.33-7.234a12.87 12.87 0 0 1 3.535 6.046 14.546 14.546 0 0 0 6.284-3.728A10.171 10.171 0 0 0 12 1.776a10.124 10.124 0 0 0-2.158 1.1zM20.364 6.5c-2.183 2.078-4.533 3.511-6.941 4.489.505 1.129.972 2.265 1.396 3.4 1.567-.305 3.623-.187 5.322.25A10.137 10.137 0 0 0 20.364 6.5z"/>',
+            'icon' => 'palette', // Lucide generic
             'label' => 'Dribbble',
             'prefix' => 'https://dribbble.com/',
         ],
         'figma' => [
-            'svg' => '<path d="M12 24c-3.314 0-6-2.686-6-6V6c0-3.314 2.686-6 6-6s6 2.686 6 6v12c0 3.314-2.686 6-6 6zM9 12a3 3 0 0 0 3 3h3v-6h-3a3 3 0 0 0-3 3zm0-6a3 3 0 0 0 3 3h3V3h-3a3 3 0 0 0-3 3zm6 12a3 3 0 0 0-3-3h-3a3 3 0 0 0 0 6h3a3 3 0 1 0 3-3z"/>',
+            'icon' => 'component', // Lucide generic
             'label' => 'Figma',
             'prefix' => 'https://www.figma.com/@',
         ],
@@ -130,6 +131,7 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://unpkg.com/lucide@latest"></script>
 
     <script>
         if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -172,8 +174,8 @@
                         class="p-2 w-8 h-8 flex items-center justify-center text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800"
                         onclick="localStorage.theme === 'dark' ? localStorage.theme = 'light' : localStorage.theme = 'dark'; if (localStorage.theme === 'dark') { document.documentElement.classList.add('dark') } else { document.documentElement.classList.remove('dark') }"
                     >
-                        <svg class="w-5 h-5 hidden dark:block" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>
-                        <svg class="w-5 h-5 block dark:hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
+                        <i data-lucide="sun" class="w-5 h-5 hidden dark:block"></i>
+                        <i data-lucide="moon" class="w-5 h-5 block dark:hidden"></i>
                     </button>
 
                     @auth
@@ -223,13 +225,13 @@
                     <div class="flex flex-wrap gap-4 text-sm text-gray-600 dark:text-gray-400">
                         @if($loc = $get('location'))
                             <div class="flex items-center gap-1.5 px-3 py-1 rounded-full bg-gray-100 dark:bg-gray-800/50">
-                                <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>
+                                <i data-lucide="map-pin" class="w-4 h-4"></i>
                                 {{ $loc }}
                             </div>
                         @endif
                         @if($email = $get('email'))
                             <a href="mailto:{{ $email }}" class="flex items-center gap-1.5 px-3 py-1 rounded-full bg-gray-100 dark:bg-gray-800/50 hover:bg-primary-50 dark:hover:bg-primary-900/20 hover:text-primary-600 transition-colors">
-                                <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
+                                <i data-lucide="mail" class="w-4 h-4"></i>
                                 {{ $email }}
                             </a>
                         @endif
@@ -240,7 +242,7 @@
                             @if($handle = $get("socials.$key"))
                                 <a href="{{ $buildUrl($key, $handle) }}" target="_blank" rel="noopener noreferrer" 
                                    class="h-10 w-10 flex items-center justify-center rounded-lg border border-gray-200 dark:border-gray-800 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white hover:border-gray-300 dark:hover:border-gray-600 transition-all bg-white dark:bg-gray-900 shadow-sm">
-                                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">{!! $conf['svg'] !!}</svg>
+                                    <i data-lucide="{{ $conf['icon'] }}" class="w-5 h-5"></i>
                                 </a>
                             @endif
                         @endforeach
@@ -343,7 +345,7 @@
                                 <div class="mt-auto pt-4 flex items-center justify-between">
                                     @if($project['url'])
                                         <span class="text-xs font-medium text-primary-600 dark:text-primary-400 flex items-center gap-1">
-                                            Visit Project <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="3"><path d="M7 17L17 7M17 7H7M17 7V17"/></svg>
+                                            Visit Project <i data-lucide="external-link" class="w-3 h-3"></i>
                                         </span>
                                     @endif
                                 </div>
@@ -385,7 +387,7 @@
                 <div class="flex items-center gap-4">
                     @if($reqCount = $get('api_request_count'))
                         <span class="font-mono text-xs opacity-70 flex items-center gap-1" title="API Hits">
-                            <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2"><path d="M13 10V3L4 14h7v7l9-11h-7z"/></svg>
+                            <i data-lucide="zap" class="w-3.5 h-3.5"></i>
                             {{ number_format($reqCount) }}
                         </span>
                     @endif
@@ -395,5 +397,10 @@
         </div>
     </footer>
 
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            lucide.createIcons();
+        });
+    </script>
 </body>
 </html>
